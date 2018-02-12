92a4703b8ac4e25ddf807722bda0bd238a276564
WarriusBird <nbgibson@crimson.ua.edu>
2018-02-11 21:02:30 -0500

    Init
    Intial commit

---
+++ Adventure/Adventure.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Engine;
+
+namespace Adventure
+{
+    public partial class Adventure : Form
+    {
+        private Player _player;
+
+        public Adventure()
+        {
+            InitializeComponent();
+
+            Location location = new Location(1, "Home", "This is your home.");
+            
+            _player = new Player(10, 10, 20, 0, 1);
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
+        }
+    }
+}
